9978ddfa2ce1817ce0ce27ef8e23cfa6d57256b4
Himanshu Shakya <hs3928782@gmail.com>
2023-07-20 13:23:35 +0530

    sum of the two number

---
+++ dsa2.js
@@ -0,0 +1,27 @@
+//add two number
+
+// let num1 = 12;
+// let num2 = 23;
+// let sum = num1+num2;
+// console.log(sum)
+
+
+
+//by function method
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(2,4));
+
+
+
+// by user
+
+let input = require('prompt-sync') ()
+
+let num1 = parseInt(input(console.log("enter the first number: ")));
+let num2 = parseInt(input(console.log("enter the second number: ")));
+let sum = num1+num2;
+console.log("sum of two number is : ",sum)
