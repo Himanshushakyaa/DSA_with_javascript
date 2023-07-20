21a1eb60c35bc511fd57d5d8bf97b5d5f6f1f91f
Himanshu Shakya <hs3928782@gmail.com>
2023-07-20 14:08:25 +0530

    find the sqaure root of number

---
+++ dsa3.js
@@ -0,0 +1,28 @@
+// find the square root 
+
+
+
+//by math.sqrt()method
+
+// let num1 = 9;
+// let squareroot = Math.sqrt(num1);
+// console.log(squareroot);
+
+
+
+//by math.pow()method
+
+let a = 9
+let sqrt = Math.pow(a,0.5)
+console.log(`sqaure root of ${a} is ${sqrt}`)
+
+
+
+//by user
+
+// let input = require('prompt-sync')();
+// let squareroot =parseInt(input(console.log("enter any number : ",)));
+// const result = Math.sqrt(squareroot);
+// console.log(`the squre root of ${squareroot} is = ${result}`);
+
+
