80dc928f19f00459a08ed59e0cc0eecd24992db5
Himanshu Shakya <hs3928782@gmail.com>
2023-07-23 16:59:34 +0530

    calculate the area of a triangle

---
+++ dsa11.js
@@ -0,0 +1,16 @@
+// program to calculate the  Area of triangle
+
+let height = 12
+let base = 13
+let Area  = (height *base)/2
+console.log(Area)
+
+
+
+//by user
+  
+let input  = require('prompt-sync')()
+let height1 = parseInt(input(console.log("enter the height triangle :")));
+let base1 = parseInt(input(console.log("enter the base of triangle :")))
+let Area1 = (height1*base1)/2
+console.log("The area of triangle is :",Area1)
