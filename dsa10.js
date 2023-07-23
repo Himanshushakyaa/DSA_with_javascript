ceebc2b8bced50a9519e1f455ed4f8ed830b7340
Himanshu Shakya <hs3928782@gmail.com>
2023-07-23 16:57:33 +0530

    find prime or not prime number

---
+++ dsa10.js
@@ -0,0 +1,24 @@
+const input =  require('prompt-sync')()
+let number = parseInt(input(console.log("enter any number")))
+let isprime = true
+if ( number === 1){
+    console.log("1 is neither prime nor composite prime")
+
+}else if(number>1){
+    for(let i = 2; i <number; i++){
+    if (number%i==0){
+        isprime = false
+        break;
+    }
+} 
+   if(isprime){
+    console.log(`${number} is prime number`)
+
+}else{
+    console.log(`${number} is a not prime number`)
+}
+}
+else{
+    console.log("The number is not a prime number ")
+}
+
