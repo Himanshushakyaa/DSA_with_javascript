a1bc2283ec69a4f79d0b6459566d6aeaf4626bec
Himanshu <hs3928782@gmail.com>
2023-08-23 15:50:42 +0530

    find the sum of natural numbers using recursion

---
+++ dsa26.js
@@ -0,0 +1,16 @@
+//find the sum of natural numbers using recursion
+let prompt = require('prompt-sync')()
+
+
+function sum(num){
+    if(num > 0){
+        return num + sum(num-1);
+
+    }else {
+        return num;
+
+    }
+}
+const number =  parseInt(prompt('Enter a positive integer :'))
+const result = sum (number);
+console.log(`The sum is ${result}`)
