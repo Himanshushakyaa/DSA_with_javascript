8f0012a81d2de484e6fed6ff95bdce935e9ab208
Himanshu <hs3928782@gmail.com>
2023-08-23 14:48:52 +0530

    number have same last digit

---
+++ dsa23.js
@@ -0,0 +1,16 @@
+let prompt = require('prompt-sync')()
+const a =  parseInt(prompt('Enter the first integer : '));
+const b = parseInt (prompt('enter the second integer:'));
+const c = parseInt (prompt('enter the third integer :'));
+
+const result1 = a % 10;
+const result2 = b % 10;
+const result3 = c % 10;
+
+if (result1 == result2 && result1 == result3){
+    console.log(`${a},${b} and ${c} have the same last digit.`)
+    
+}
+else {
+    console.log(`${a},${b} amd ${c} have different last digit.`)
+}
