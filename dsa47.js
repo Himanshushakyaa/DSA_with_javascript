09f3391da6304be5e90c4612b81ec9441f3af467
Himanshu <hs3928782@gmail.com>
2023-10-08 09:24:54 +0530

    create a multiline string

---
+++ dsa47.js
@@ -0,0 +1,23 @@
+//program to create a multiline string
+
+//by using the +operator
+
+const msg = 'This is a long message\n'+ 'that spans across multiple lines\n'+
+'in the code.'
+console.log(msg)
+
+
+//create multiline strings
+
+const message = 'This is a long message\n\
+that spans across multiple lines\n\
+in the code'
+console.log(message)
+
+
+//by using template literal
+
+const message1 = `this is a long message
+that spans across multiple lines
+in the code`
+console.log(message1)
