40df407952c86f0acad4484ef4458d275f180d9c
Himanshu <hs3928782@gmail.com>
2023-10-08 09:21:03 +0530

    add to a key to an object

---
+++ dsa45.js
@@ -0,0 +1,26 @@
+//program to add a key /value pair to an object
+
+
+// const person  = {
+//     name:'Himanshu',
+//     age:22,
+//     gender:'male'
+// }
+// person.height = 5.4;
+// console.log(person);
+
+
+
+
+
+// by using square Bracket notation
+
+const person = {
+    name :'Himanshu',
+    age:22,
+    gender:'male'
+}
+
+person['height'] = 5.4;
+
+console.log(person);
